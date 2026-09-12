fb2effe953bcdd39b0c2a4cd66d472cdbb349797
Dmitryqqw <legendaritydimitry@gmail.com>
2026-09-12 11:18:16 +0300

    2 выполненых задания

--- MDK_01.01_Lab_Practic_Funtion/MDK_01.01_Lab_Practic_Funtion/Program.cs
+++ MDK_01.01_Lab_Practic_Funtion/MDK_01.01_Lab_Practic_Funtion/Program.cs
@@ -1,15 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MDK_01._01_Lab_Practic_Funtion
+class Program
 {
-    internal class Program
+    
+    static void Main()
     {
-        static void Main(string[] args)
+        //задание 1
+        //Console.Write("Введите натуральное число: ");
+        //long number = ReadNaturalNumber();
+        //long sum = SumOfDigits(number);
+        //Console.WriteLine($"Сумма цифр числа {number} равна {sum}.");
+
+        Console.Write("Введите натуральное чисилио: ");
+        long number = ReadNaturalNumber();
+        long reversed = ReverseNumber(number);
+        PrintResult(reversed);
+
+
+    }
+
+  
+    //static long ReadNaturalNumber()
+    //{
+    //    while (true)
+    //    {
+    //        string input = Console.ReadLine();
+    //        if (TryParseNatural(input, out long value))
+    //            return value;
+
+    //        Console.Write("Некорректный ввод. Введите натуральное число: ");
+    //    }
+    //}
+
+ 
+    //static bool TryParseNatural(string input, out long value)
+    //{
+    //    value = 0;
+    //    if (string.IsNullOrEmpty(input)) return false;
+
+    //    foreach (char c in input)
+    //    {
+    //        if (c < '0' || c > '9') return false;
+    //        value = value * 10 + (c - '0');
+    //    }
+    //    return value > 0;
+    //}
+
+
+    //static long SumOfDigits(long number)
+    //{
+    //    long sum = 0;
+    //    while (number > 0)
+    //    {
+    //        sum += number % 10;
+    //        number /= 10;
+    //    }
+    //    return sum;
+    //}
+
+
+
+
+    static long ReadNaturalNumber()
+    {
+        while (true)
         {
+            string input = Console.ReadLine();
+            if (TryParseNatural(input, out long value))
+                return value;
+
+            Console.Write("Ты дурак ┏(-_-)┛┗(-_-﻿ )┓┗(-_-)┛┏(-_-)┓. Введите натуральное число: ");
         }
     }
+    static bool TryParseNatural(string input, out long value)
+    {
+        value = 0;
+        if (string.IsNullOrEmpty(input)) return false;
+
+        foreach (char c in input)
+        {
+            if (c < '0' || c > '9') return false;
+            value = value * 10 + (c - '0');
+        }
+        return value > 0; 
+    }
+    static long ReverseNumber(long number)
+    {
+        long reversed = 0;
+        while (number > 0)
+        {
+            reversed = reversed * 10 + number % 10;
+            number /= 10;
+        }
+        return reversed;
+    }
+    static void PrintResult(long reversed)
+    {
+        Console.WriteLine($"Числа задым напырод : {reversed}");
+    }
 }
+
