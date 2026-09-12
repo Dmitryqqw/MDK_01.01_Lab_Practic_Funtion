33ed0fa64608f21ca33093466eeab80eacc955af
Dmitryqqw <legendaritydimitry@gmail.com>
2026-09-12 10:35:23 +0300

    start

---
+++ MDK_01.01_Lab_Practic_Funtion/MDK_01.01_Lab_Practic_Funtion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDK_01._01_Lab_Practic_Funtion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
